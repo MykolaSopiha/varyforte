a8f65c66f00d1878859ae1a25b253fb3ff02b81e
Sopiha Mykola <niksop94@gmail.com>
2017-08-30 19:29:36 +0300

    color-box needs to be fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,12 @@
 import $ from 'jquery';
 import 'slick-carousel';
 
+
 // WEB FONT LOADER begin
 var WebFont = require('webfontloader');
 WebFont.load({
 	google: {
-		families: ['Acme:400']
+		families: ['Acme:400', 'Catamaran:700']
 	}
 });
 // WEB FONT LOADER end
@@ -13,27 +14,47 @@ WebFont.load({
 
 $(document).ready(function () {
 
-	// $('.slick-carousel').each(function () {
-	// 	$('this').find('.slides').slick({
-	// 		nextArrow: $('this').find('.arrow-next'),
-	// 		prevArrow: $('this').find('.arrow-prev')
-	// 	});
-	// });
-
-
 	// --- slick.js initialization begin ---
 	$('.slick-carousel').each(function (idx, item) {
 	    var carouselId = "carousel" + idx;
-	    $(this).find('.slides').id = carouselId + "items";
-	    $(this).find('.arrow-prev').id = carouselId + "arrow-prev";
-	    $(this).find('.arrow-next').id = carouselId + "arrow-next";
+	    $(this).find('.slider__items').id = carouselId + "items";
+	    $(this).find('.slider__arrow--prev').id = carouselId + "arrow--prev";
+	    $(this).find('.slider__arrow--next').id = carouselId + "arrow--next";
 
-	    $(this).find('.slides').slick({
-			prevArrow: $(this).find('.arrow-prev'),
-			nextArrow: $(this).find('.arrow-next')
+	    $(this).find('.slider__items').slick({
+			prevArrow: $(this).find('.slider__arrow--prev'),
+			nextArrow: $(this).find('.slider__arrow--next'),
+			adaptiveHeight: true,
+			infinite: true,
+			speed: 300,
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			responsive: [
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 1,
+						infinite: true,
+					}
+				},
+				{
+					breakpoint: 600,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 1
+					}
+				},
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
+					}
+				}
+			]
 	    });
 	});
 	// --- slick.js initialization end ---
-
 
 });
