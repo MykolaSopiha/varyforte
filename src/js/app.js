9a84fa20e762f49f871df61159d3573b278711b1
Mykola Sopiha <niksop94@gmail.com>
2017-10-24 14:12:53 +0300

    new commit

--- src/js/app.js
+++ src/js/app.js
@@ -35,8 +35,32 @@ $(document).ready(function () {
 			adaptiveHeight: false,
 			infinite: true,
 			speed: 300,
-			slidesToShow: 1,
+			slidesToShow: 4,
 			slidesToScroll: 1,
+			responsive: [
+			{
+				breakpoint: 1366,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					infinite: true
+				}
+			},
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					infinite: true
+				}
+			},
+			{
+				breakpoint: 663,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}]
 	    });
 	});
 	// --- slick.js initialization end ---
